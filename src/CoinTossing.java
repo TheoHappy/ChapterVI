d5bdc6cc60e98bee2ea620dfad9bd0834fd7af69
TheoHappy <38621790+TheoHappy@users.noreply.github.com>
2019-12-03 23:38:54 +0300

    I finished task 29

--- src/CoinTossing.java
+++ src/CoinTossing.java
@@ -1,5 +1,41 @@
-public class CoinTossing {
-    public static void main(String[] args) {
+import java.security.SecureRandom;
+import java.util.Scanner;
 
+public class CoinTossing {
+    private enum Coin {HEAD, TAIL}
+
+    ;
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("*************************Menu************************");
+        System.out.println();
+
+        int switcher;
+        int tailFrequency = 0;
+        int headFrequency = 0;
+
+        do {
+            System.out.println("Enter 0 for toss coin / or enter other digit for exit");
+            switcher = input.nextInt();
+            if (switcher != 0)
+                break;
+            Coin coinValue = flip();
+            System.out.println("Result of coin tossing is " + coinValue);
+            if (coinValue == Coin.HEAD) {
+                headFrequency++;
+            } else {
+                tailFrequency++;
+            }
+        } while (switcher == 0);
+
+        System.out.println("HEAD appears " + headFrequency + " times");
+        System.out.println("TAIL appears " + tailFrequency + " times");
+    }
+
+    public static Coin flip() {
+        SecureRandom randomFlip = new SecureRandom();
+        int flipValue = randomFlip.nextInt(2);
+        return flipValue == 1 ? Coin.TAIL : Coin.HEAD;
     }
 }
